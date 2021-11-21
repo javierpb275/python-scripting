3033a4c26aab3cd772b5bc3a5b835731ea9b4a64
javierpb275 <javierpb275@gmail.com>
2021-11-21 18:19:38 +0100

    images with python 3: thumbnail method

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from PIL import Image, ImageFilter
 
 if __name__ == '__main__':
-    img = Image.open('./Pokedex/pikachu.jpg')
-    filtered_img = img.convert('L')
-    #crooked = filtered_img.rotate(90)
-    # crooked.save("grey.png", 'png')
-    #resize = filtered_img.resize((300, 300))
-    #resize.save("grey.png", 'png')
+    img = Image.open('./astro.jpg')
+    img.thumbnail((400, 400))
+    img.save('thumbnail.jpg')
 
-    box = (100, 100, 400, 400)
-    region = filtered_img.crop(box)
-    region.save("grey.png", 'png')
-
-    region.show()#show image
+    print(img.size)#(400, 381)
 
 
