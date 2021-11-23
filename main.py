5a5c409b08ba5cb3a5f04a107c4dd87b3f2a9f83
javierpb275 <javierpb275@gmail.com>
2021-11-23 17:37:25 +0100

    exercise pdf merger

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import PyPDF2
+import sys
+
+inputs = sys.argv[1:]#grab all we write in the console into a list of inputs
 
 if __name__ == '__main__':
-    with open('dummy.pdf', 'rb') as file:
-        print(file)#<_io.TextIOWrapper name='dummy.pdf' mode='r' encoding='cp1252'>
-        reader = PyPDF2.PdfFileReader(file)
-        print(reader.numPages)#1
-        page = reader.getPage(0)
-        print(page)#{'/Type': '/Page', '/Parent': IndirectObject(4, 0)...
-        page.rotateCounterClockwise(90)
-        writer = PyPDF2.PdfFileWriter()
-        writer.addPage(page)
-        with open('tilt.pdf', 'wb') as new_file:
-            writer.write(new_file)
+    def pdf_combiner(pdf_list):
+        merger = PyPDF2.PdfFileMerger()
+        for pdf in pdf_list:
+            print(pdf)
+            merger.append(pdf)
+            merger.write('super.pdf')
 
+    pdf_combiner(inputs)
 
