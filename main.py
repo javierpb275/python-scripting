736e6aa019b155e4ffbd5f7a454f7fe25670f51f
javierpb275 <javierpb275@gmail.com>
2021-11-23 18:39:20 +0100

    sending emails with python and using html

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
 if __name__ == '__main__':
+    html = Template(Path('index.html').read_text())
     email = EmailMessage()
     email['from'] = 'Python'
     email['to'] = 'pepe@gmail.com'
-    email['subject'] = 'hi i am python'
+    email['subject'] = 'Hi! My name is Python!'
 
-    email.set_content('i am sending you an email')
+    email.set_content(html.substitute({'name': 'Nice to meet you!'}), 'html')
 
     with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
         smtp.ehlo()
         smtp.starttls()
-        smtp.login('python@gmail.com', 'python')
+        smtp.login('python@gmail.com', 'python123.')
         smtp.send_message(email)
         print('OK')
 
