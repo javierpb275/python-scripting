badbb2132451b8c9a700e2c78488ba3226d1cef9
javierpb275 <javierpb275@gmail.com>
2021-11-23 17:51:22 +0100

    exercise watermarker

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import PyPDF2
-import sys
-
-inputs = sys.argv[1:]#grab all we write in the console into a list of inputs
 
 if __name__ == '__main__':
-    def pdf_combiner(pdf_list):
-        merger = PyPDF2.PdfFileMerger()
-        for pdf in pdf_list:
-            print(pdf)
-            merger.append(pdf)
-            merger.write('super.pdf')
+    template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
+    watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+    output = PyPDF2.PdfFileWriter()
 
-    pdf_combiner(inputs)
+    for i in range(template.getNumPages()):
+        page = template.getPage(i)
+        page.mergePage(watermark.getPage(0))
+        output.addPage(page)
+
+        with open('watermarked_output.pdf', 'wb') as file:
+            output.write(file)
 
